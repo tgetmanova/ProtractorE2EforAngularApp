38b6472a13d2d9c7e30a82857e6962e3ba201296
tgetmanova <tatyana.getmanova@gmail.com>
2018-04-01 16:15:54 +0300

    Add Reporting and text attachments

--- e2e/steps/Step.js
+++ e2e/steps/Step.js
@@ -1,6 +1,8 @@
 const DashboardContext = require('./DashboardContext').DashboardContext;
 const HeroContext = require('./HeroContext').HeroContext;
+
 const Random = require('../utils/Random').Random;
+const Reporting = require('../utils/ReportUtils').ReportUtils;
 
 class Step {
 
@@ -13,12 +15,15 @@ class Step {
     this.dashboardContext.openFromTopNavigation();
     this.heroNameContext = this.dashboardContext.getTopHeroesNames()
       .then(names => {
-        return Random.getRandomElement(names);
+        let randomHeroName = Random.getRandomElement(names);
+        Reporting.addStep(`Selecting random Top Hero: [${randomHeroName}] from Top Hero list: [${names}]`);
+        Reporting.attachText('Initial Top Heroes List', names.join(', '));
+        return randomHeroName;
       });
   }
 
   deleteHero() {
-    this.heroContext.openHeroesList()
+    this.heroContext.openHeroesList();
     this.heroNameContext
       .then(heroName => {
         this.heroContext.deleteHero(heroName);
@@ -32,7 +37,10 @@ class Step {
         this.dashboardContext.openFromTopNavigation();
         this.dashboardContext.getTopHeroesNames()
           .then(names => {
-            expect(names).not.toContain(name);
+            Reporting.addStep(`Verifying [${name}] is not displayed within Top Heroes list: [${names}]`, () => {
+              expect(names).not.toContain(name);
+            });
+            Reporting.attachText('Final Top Heroes List', names.join(', '));
           });
       });
   }
--- e2e/utils/ReportUtils.js
+++ e2e/utils/ReportUtils.js
@@ -1,11 +1,24 @@
 class ReportUtils {
 
   static attachScreenshot(descriptionLabel) {
-    browser.takeScreenshot().then(function(png) {
+    browser.takeScreenshot().then(png => {
       allure.createAttachment(descriptionLabel, function () {
         return new Buffer(png, 'base64');
       }, 'image/png')();
     });
+  }
+
+  static addStep(description, action) {
+    if (action === undefined) {
+      action = () => { };
+    }
+    allure.createStep(description, action)();
+  }
+
+  static attachText(description, text) {
+    allure.createAttachment(description, () => {
+      return new Buffer(text, 'UTF-8');
+    }, 'text/plain')();
   }
 }
 
